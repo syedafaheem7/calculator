4eaadaca554937bb86eb8602687a3dae54856a44
syedafaheem7 <45543815+syedafaheem7@users.noreply.github.com>
2019-05-22 14:56:54 -0400

    Add files via upload

---
+++ IndividualProject/src/edu/qc/seclass/replace/MainTestSuite.java
@@ -0,0 +1,11 @@
+package edu.qc.seclass.replace;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MainTest.class, MainTestAddOn.class, MyMainTest.class })
+public class MainTestSuite {
+
+}
